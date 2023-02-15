e11212fc9e7edcb042ea286f609977b365feabc5
Nergül <nergulkahya019@gmail.com>
2023-02-15 14:20:53 +0300

    dgdfg

---
+++ manuelvalueentry.api.database.contract/Models/FactoryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manuelvalueentry.Api.Database.Contract.Models
+{
+    internal class FactoryModel
+    {
+    }
+}
